55c7b182573e704074f9b88dab18604e2d91190f
Einarr07 <aidenperse@gmail.com>
2025-11-25 16:15:42 -0500

    Models
    - main: Created new enpoints for the new classes
    - models: I created the new classes to simulate the enpoints

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+from typing import List
 from zoneinfo import ZoneInfo
 
 from fastapi import FastAPI, HTTPException
+
+from models import Transaction, Invoice, CustomerCreate, Customer
 
 app = FastAPI()
 
@@ -9,6 +12,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {'Message': 'Hello World'}
+
 
 country_timezones = {
     "CO": "America/Bogota",
@@ -18,6 +22,7 @@ country_timezones = {
     "AR": "America/Argentina/Buenos_Aires",
 }
 
+
 @app.get("/get-time/{iso_code}")
 async def get_time(iso_code: str):
     iso = iso_code.upper()
@@ -25,6 +30,7 @@ async def get_time(iso_code: str):
     tz = ZoneInfo(timezone_str)
     now = datetime.now(tz)
     return f'La hora en {timezone_str} es: {now.isoformat()}'
+
 
 @app.get("/format/{frt}")
 async def get_hour(frt: str, iso_code: str):
@@ -50,3 +56,28 @@ async def get_hour(frt: str, iso_code: str):
     }
 
 
+db_simulator: list[Customer] = []
+
+
+@app.get("/customers", response_model=List[Customer])
+async def list_customers():
+    return db_simulator
+
+
+@app.post("/customers", response_model=Customer)
+async def create_customer(customer_data: CustomerCreate):
+    customer = Customer.model_validate(customer_data.model_dump())
+    # Asumeindo que se hace en la db
+    db_simulator.append(customer)
+    customer.id = len(db_simulator)
+    return customer
+
+
+@app.post("/transaction")
+async def create_transaction(transaction_data: Transaction):
+    return transaction_data
+
+
+@app.post("/invoices")
+async def create_invoices(invoice_data: Invoice):
+    return invoice_data
